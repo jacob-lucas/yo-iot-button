6b48824dbe0c7d022f86ae5de9b0c687c144ca1d
Jacob Lucas <jacob.lucas@grab.com>
2017-03-05 21:07:53 -0800

    Adding code from Yo API docs for the Yo Request Sender

---
+++ src/main/java/com/jacoblucas/yo/YoRequestSender.java
@@ -0,0 +1,45 @@
+package com.jacoblucas.yo;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+class YoRequestSender {
+
+    /**
+     * Post a Yo request.
+     * Reproduced from http://docs.justyo.co/docs/getting-started.
+     * @param url The Yo API URL to post the request to.
+     * @param req The Yo request.
+     * @return A String representation of the raw response.
+     * @throws IOException in case of error posting the Yo request.
+     */
+    String post(String url, YoRequest req) throws IOException {
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        con.setRequestMethod("POST");
+
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes("api_token=" + req.getApiKey() + "&username=" + req.getUsername());
+        wr.flush();
+        wr.close();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        return response.toString();
+    }
+
+}
